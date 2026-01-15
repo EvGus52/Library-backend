4468adc1f10e2f427f5b4e8c5bae89f7bed00118
EvGus52 <evgenijguskov247@gmail.com>
2026-01-15 18:05:23 +0300

    добавил MongoDB на основе REST API по пользователям

--- src/app.js
+++ src/app.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const usersRouter = require("./routes/users");
 const loggerOne = require("./middlewares/loggerOne");
 
 dotenv.config();
 
-const app = express();
+const {
+  PORT = 3003,
+  API_URL = "http://127.0.0.1",
+  MONGO_URL = "mongodb://localhost:27017/backend",
+} = process.env;
 
-const { PORT = 3003, API_URL = "http://127.0.0.1" } = process.env;
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDb");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
+
+const app = express();
 
 const helloWorld = (request, response) => {
   response.status(200);
